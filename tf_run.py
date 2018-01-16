efba95dcf5380c40961433bedaec86fb0e69583d
dallascard <dcard@andrew.cmu.edu>
2018-01-16 13:43:56 -0800

    debug

--- tf_run.py
+++ tf_run.py
@@ -108,6 +108,9 @@ def train(network_architecture, minibatches, type='prodlda',learning_rate=0.001,
 
 def print_top_words(beta, feature_names, n_top_words=10):
     print '---------------Printing the Topics------------------'
+    temp = beta[0].copy()
+    temp = temp.sort()
+    print(temp[:-n_top_words - 1:-1])
     for i in range(len(beta)):
         print(" ".join([feature_names[j]
             for j in beta[i].argsort()[:-n_top_words - 1:-1]]))
