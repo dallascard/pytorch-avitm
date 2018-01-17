1c8dd5d6ec05448e2ea1fbdfa8314be7c25a17e0
dallascard <dcard@andrew.cmu.edu>
2018-01-17 15:29:12 -0800

    trying per-weight regularizer

--- tf_model.py
+++ tf_model.py
@@ -131,7 +131,8 @@ class VAE(object):
         regularizer = tf.nn.l2_loss(self.network_weights['beta'])
         #self.cost = tf.add(tf.reduce_mean(NL + KLD), tf.reduce_mean(tf.reduce_sum(regularizer), 1))
         #self.cost = tf.add(tf.reduce_mean(NL + KLD), 1.0 * regularizer)
-        self.cost = tf.add(tf.reduce_mean(NL + KLD), tf.reduce_sum(0.5 * tf.square(self.network_weights['beta'])))
+        #self.cost = tf.add(tf.reduce_mean(NL + KLD), tf.reduce_sum(0.5 * tf.square(self.network_weights['beta'])))
+        self.cost = tf.add(tf.reduce_mean(NL + KLD), tf.reduce_sum(0.5 * tf.multiply(self.l2_strength, tf.square(self.network_weights['beta']))))
 
         #self.cost = tf.reduce_mean(NL + KLD)
 
--- tf_run.py
+++ tf_run.py
@@ -93,7 +93,7 @@ def train(network_architecture, minibatches, type='prodlda',learning_rate=0.001,
     bg = 0
 
     total_batch = int(n_samples_tr / batch_size)
-    l2_strength = np.ones([network_architecture["n_z"], network_architecture["n_hidden_gener_1"]]) / float(total_batch)
+    l2_strength = np.ones([network_architecture["n_z"], network_architecture["n_hidden_gener_1"]]) / float(n_samples_tr)
 
     # Training cycle
     for epoch in range(training_epochs):
@@ -118,7 +118,7 @@ def train(network_architecture, minibatches, type='prodlda',learning_rate=0.001,
             weights_sq = weights ** 2
             weights_sq[weights_sq < min_weights_sq] = min_weights_sq
 
-            l2_strength = 0.5 / weights_sq / float(n_samples_tr)
+            l2_strength = weights_sq / float(n_samples_tr)
 
         # Display logs per epoch step
         if epoch % display_step == 0:
