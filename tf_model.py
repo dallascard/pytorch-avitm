0853d763c28c9edfa252aeed0c25c54b4bf57d25
dallascard <dcard@andrew.cmu.edu>
2018-01-16 17:51:48 -0800

    debug

--- tf_model.py
+++ tf_model.py
@@ -26,13 +26,13 @@ class VAE(object):
     See "Auto-Encoding Variational Bayes" by Kingma and Welling for more details.
     """
     def __init__(self, network_architecture, transfer_fct=tf.nn.softplus,
-                 learning_rate=0.001, batch_size=100, init_bg=None):
+                 learning_rate=0.001, batch_size=100, init_bg=None, init_l2_strength=0.01):
         self.network_architecture = network_architecture
         self.transfer_fct = transfer_fct
         self.learning_rate = learning_rate
         self.batch_size = batch_size
-        #self.l2_strength = l2_strength
-        self.l2_strength = tf.placeholder(tf.float32, name="l2_strength")
+        self.l2_strength = init_l2_strength
+        #self.l2_strength = tf.placeholder(tf.float32, name="l2_strength")
         print 'Learning Rate:', self.learning_rate
 
         # tf Graph input
@@ -128,7 +128,7 @@ class VAE(object):
         #self.cost = tf.reduce_mean(NL + KLD + regularization_penalty)
 
         regularizer = tf.nn.l2_loss(self.network_weights['beta'])
-        self.cost = tf.reduce_mean(NL + KLD + self.l2_strength * regularizer + tf.reduce_sum(tf.zeros_like(regularizer) * regularizer))
+        self.cost = tf.reduce_mean(NL + KLD + tf.reduce_sum(self.l2_strength * tf.ones_like(regularizer) * regularizer))
 
         #self.cost = tf.reduce_mean(NL + KLD)
 
@@ -144,17 +144,17 @@ class VAE(object):
         decoder_weight = self.network_weights['beta']
         background = self.network_weights['background']
 
-        opt, cost, emb, bg = self.sess.run((self.optimizer, self.cost, decoder_weight, background), feed_dict={self.x: X, self.keep_prob: .8, self.l2_strength: 0.0000})
+        opt, cost, emb, bg = self.sess.run((self.optimizer, self.cost, decoder_weight, background), feed_dict={self.x: X, self.keep_prob: .8, self.l2_strength: 0.0001})
         return cost, emb, bg
 
     def test(self, X):
         """Test the model and return the lowerbound on the log-likelihood.
         """
-        cost = self.sess.run((self.cost),feed_dict={self.x: np.expand_dims(X, axis=0),self.keep_prob: 1.0, self.l2_strength: 0.0001})
+        cost = self.sess.run((self.cost),feed_dict={self.x: np.expand_dims(X, axis=0),self.keep_prob: 1.0})
         return cost
 
     def topic_prop(self, X):
         """heta_ is the topic proportion vector. Apply softmax transformation to it before use.
         """
-        theta_ = self.sess.run((self.z),feed_dict={self.x: np.expand_dims(X, axis=0),self.keep_prob: 1.0, self.l2_strength: 0.0001})
+        theta_ = self.sess.run((self.z),feed_dict={self.x: np.expand_dims(X, axis=0),self.keep_prob: 1.0})
         return theta_
